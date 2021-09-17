c2019811bc255411739a3d8379aa07aaf044e617
Dante Pileckas <pileckas33@gmail.com>
2021-09-17 01:45:31 -0300

    header and styles

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './componentes/header';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header
+     titulo={'Administrador de Pacientes de Veterinaria'}
+     />
+    </div>
+  );
+}
+
+export default App;
