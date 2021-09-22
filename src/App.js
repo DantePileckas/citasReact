b9a5af13f9de9d5d436f32060e393311d7d5b7e2
Dante Pileckas <pileckas33@gmail.com>
2021-09-22 16:33:31 -0300

    Funciones de formulario y app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import Header from './componentes/header';
-
+import React, {Componenet} from 'react';
+import Header from './componentes/Header';
+import AgregarCita from './componentes/AgregarCitas';
 
 function App() {
+
+
+  crearCita = () => {
+    
+  }
+
   return (
-    <div className="App">
+    <div className="container">
      <Header
      titulo={'Administrador de Pacientes de Veterinaria'}
      />
+     <div className="row">
+       <div className="col-md-6">
+         <AgregarCita
+         crearCita={this.crearCita}
+         />
+       </div>
+     </div>
     </div>
   );
 }
-
 export default App;
